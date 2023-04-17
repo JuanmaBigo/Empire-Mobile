9df5445b91d27fbcff5913f314e9d942abd65bd3
RominaGaite <gaiteromina@gmaul.com>
2023-04-17 10:01:58 -0300

    .

--- store/model/actions.js
+++ store/model/actions.js
@@ -22,21 +22,10 @@ const getAllCars = createAsyncThunk(
     }
   });
 
-<<<<<<< HEAD
-
-  
-
-  const getOne = createAsyncThunk('getOne', async ({_id})=>{
-    try{
-      const response = await axios.get(`https://empire-back.onrender.com/api/cars/${_id}`)
-
-      //console.log(response)
-=======
   const getOne = createAsyncThunk('getOne', async ({_id})=>{
     try{
       let url = `${REACT_APP_URL}cars/${_id}`;
       const response = await axios.get(url)
->>>>>>> 1688db9b8c7a1fbaa4455e4eeec7cdc3f6341b77
       return{
         cars: response.data.response,
         
