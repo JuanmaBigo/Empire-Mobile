b647917a30a7107093027cafb1528b177d43a42b
RominaGaite <73299120+RominaGaite@users.noreply.github.com>
2023-04-17 11:33:03 -0300

    Select vehicles- filtros funcionando  (#23)
    * portadas
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * seleccion de vehiculos funcionando
    
    * seleccion de vehiculos funcionando
    
    ---------
    
    Co-authored-by: RominaGaite <gaiteromina@gmaul.com>

--- navigation/AuthStack.jsx
+++ navigation/AuthStack.jsx
@@ -94,9 +94,8 @@ export default function AppStack() {
             <Drawer.Screen name="AI ENGINE" component={AIStack} />
             <Drawer.Screen name="LOGIN" component={Login} />
             <Drawer.Screen name="REGISTER" component={Register} />
-
-
-            {/* <Drawer.Screen name="CUSTOM" component={Custom} /> */}
+            <Drawer.Screen name="CUSTOM" component={Custom} />
+            {/* <Drawer.Screen name="VIDEO" component={Videito} /> */}
 
 
         </Drawer.Navigator>
