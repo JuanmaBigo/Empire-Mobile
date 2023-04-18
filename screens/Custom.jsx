422eb9e70e98df4bee55ba92417be696a1b8cad0
Juan Mateo Bigoritto <66440535+JuanmaBigo@users.noreply.github.com>
2023-04-17 23:10:03 -0300

    Custom page (#26)
    * basic config
    
    * deleted unused files
    
    * changed background photos
    
    * prueba de video
    
    * AIEngine screen
    
    * basic config custom page
    
    * fix
    
    * changed name of services to contact us
    
    * merge
    
    * merge
    
    * store updated
    
    * progress in details page
    
    * Details Page ready
    
    * added AI Engine to auth stack
    
    * added video to museum page
    
    * styles select model

--- screens/Custom.jsx
+++ screens/Custom.jsx
@@ -66,148 +66,137 @@ export default function Custom(props) {
                 setSelectedColor(colors[0]?._id)
                 setSelectedRim(rims[0]?._id)
                 setPhotoVehicle(rims[0]?.photo)
+                setSelectedOptionRim('option4')
                 setLoaded5(true)
             } else {
                 setLoaded3(!loaded3)
             }
-        }, [selectedColor, selectedRim, loaded2])
+        }, [loaded2])
     );
 
     let colors = useSelector(store => store.colors?.colors)
     let rims = useSelector(store => store.rim?.rim)
     let car = useSelector(store => store.model?.car)
-    // console.log(rims)
-
-    const handleOptionChange = (event) => {
-        setSelectedOption(event.target.value);
-        setSelectedColor(event.target.id)
-    }
 
     if (loaded2 === true) {
         setTimeout(() => {
-            setPhotoVehicle(rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 1]?.photo)
-        }, 300);
+        setPhotoVehicle(rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 4]?.photo)
+    }, 300);
+}
+
+
+
+const color1 = { uri: colors[0]?.color_code }
+const color2 = { uri: colors[1]?.color_code }
+const color3 = { uri: colors[2]?.color_code }
+const optionsGroup1 = [
+    { label: '', value: 'option1', id: colors[0]?._id },
+    { label: '', value: 'option2', id: colors[1]?._id },
+    { label: '', value: 'option3', id: colors[2]?._id },
+];
+
+const optionsGroup2 = [
+    { label: '', value: 'option4', id: rims[0]?._id },
+    { label: '', value: 'option5', id: rims[1]?._id },
+    { label: '', value: 'option6', id: rims[2]?._id },
+];
+
+
+const [valueGroup1, setValueGroup1] = useState(colors[0]?.id || null);
+const [valueGroup2, setValueGroup2] = useState(rims[0]?.id || null);
+
+const handlePressGroup1 = (option) => {
+    setValueGroup1(option.id);
+    setSelectedColor(option.id);
+};
+
+const handlePressGroup2 = (option) => {
+    setValueGroup2(option.id);
+    setSelectedRim(option.id);
+    setSelectedOptionRim(option.value)
+};
+const radioButtonStyle = {
+    radioButton: {
+        color: 'transparent'
+    }
+};
+
+let token = AsyncStorage.getItem('token');
+let headers = { headers: { 'Authorization': `Bearer ${token}` } };
+let url = `${REACT_APP_URL}items`;
+
+async function handleItem() {
+    let data = {
+        car_id: car_id,
+        color_id: selectedColor,
+        rim_id: selectedRim
     }
 
-    const handleOptionChangeRims = (event) => {
-        setSelectedOptionRim(event.target.value);
-        setSelectedRim(event.target.id)
-    };
+    // try {
+    //     await axios.post(url, data, headers)
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    console.log(data)
 
-    let token = AsyncStorage.getItem('token');
-    let headers = { headers: { 'Authorization': `Bearer ${token}` } };
-    let url = `${REACT_APP_URL}items`;
-
-    async function handleItem() {
-        let data = {
-            car_id: car_id,
-            color_id: selectedColor,
-            rim_id: selectedRim
-        }
-
-        try {
-            await axios.post(url, data, headers)
-        } catch (error) {
-            console.log(error)
-        }
-        console.log(data)
-
-    }
-
-    const color1 = { uri: colors[0]?.color_code }
-    const color2 = { uri: colors[1]?.color_code }
-    const color3 = { uri: colors[2]?.color_code }
-    const optionsGroup1 = [
-        { label: '', value: 'option1', id: colors[0]?._id },
-        { label: '', value: 'option2', id: colors[1]?._id },
-        { label: '', value: 'option3', id: colors[2]?._id },
-    ];
-
-    const optionsGroup2 = [
-        { label: '', value: 'option4', id: rims[0]?._id },
-        { label: '', value: 'option5', id: rims[1]?._id },
-        { label: '', value: 'option6', id: rims[2]?._id },
-    ];
-
-
-    const [valueGroup1, setValueGroup1] = useState(colors[0]?.id || null);
-    const [valueGroup2, setValueGroup2] = useState(rims[0]?.id || null);
-
-    const handlePressGroup1 = (option) => {
-        setValueGroup1(option.id);
-        setSelectedColor(option.id);
-    };
-
-    const handlePressGroup2 = (option) => {
-        setValueGroup2(option.id);
-        setSelectedRim(option.id);
-        setSelectedOptionRim(option.label)
-    };
-    const radioButtonStyle = {
-        radioButton: {
-          color: 'transparent'
-        }
-      };
-    return (
-        <ScrollView style={{ backgroundColor: 'rgb(237, 237, 237)', height: '100%', width: '100%' }}>
+}
+return (
+    <ScrollView style={{ backgroundColor: 'rgb(237, 237, 237)', height: '100%', width: '100%' }}>
+        <View >
             <View >
                 <View >
-                    <View >
-                        <Image source={img} alt="make it" style={{ width: 300, height: 150, marginTop: 50, marginLeft: 50 }} />
-                    </View>
-                        {loaded5 ?
-                            <View style={{ display: 'flex', flexDirection: 'column' }}>
-                                <Image source={{ uri: photoVehicle }} alt="make it" style={{ width: 400, height: 200 }} />
+                    <Image source={img} alt="make it" style={{ width: 300, height: 150, marginTop: 50, marginLeft: 50 }} />
+                </View>
+                {loaded5 ?
+                    <View style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Image source={{ uri: photoVehicle }} alt="make it" style={{ width: 600, height: 200, alignSelf: 'center' }} />
 
-                                <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5 }}>COLOR SELECTION</Text>
-                                
-                                <View style={{ marginBottom: 20 }}>
+                        <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, paddingLeft: 20 }}>COLOR SELECTION</Text>
 
-                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', width: 400,height:150 }}>
-                                        {optionsGroup1.map((option) => (
-                                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                                                <View style={{position:'relative'}}>
-                                                <Image source={{ uri: colors.find(color => color._id === option.id)?.color_code }} alt={colors[0]?.name} style={{ width: 120, height: 120, marginTop: -30 , position:'absolute',marginLeft:-25}}  />
-                                                    <RadioButton.Item key={option.id} label={option.label} value={option.id} status={valueGroup1 === option.id ? 'checked' : 'unchecked'} onPress={() => handlePressGroup1(option)} style={radioButtonStyle.radioButton}/>
-                                                    
-                                                </View>
-                                            </View>
-                                        ))}
+                        <View style={{ marginBottom: 20 }}>
+
+                            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', width: 400, height: 150 }}>
+                                {optionsGroup1.map((option) => (
+                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                        <View style={{ position: 'relative' }}>
+                                            <Image source={{ uri: colors.find(color => color._id === option.id)?.color_code }} alt={colors[0]?.name} style={{ width: 120, height: 120, marginTop: -30, position: 'absolute', marginLeft: -25, objectFit: 'contain' }} />
+                                            <RadioButton.Item key={option.id} label={option.label} value={option.id} status={valueGroup1 === option.id ? 'checked' : 'unchecked'} onPress={() => handlePressGroup1(option)} style={radioButtonStyle.radioButton} />
+                                        </View>
+                                    </View>
+                                ))}
+                            </View>
+                        </View>
+                        <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'right', marginTop: 0, paddingRight: 30 }}>RIMS SELECTION</Text>
+                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', width: 400, height: 180 }}>
+                            {optionsGroup2.map((option) => (
+                                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                    <View style={{ position: 'relative' }}>
+                                        <Image source={{ uri: rims.find(rim => rim._id === option.id)?.photo_select }} alt={colors[0]?.name} style={{ width: 120, height: 120, marginTop: -30, position: 'absolute', marginLeft: -25 }} />
+                                        <RadioButton.Item key={option.id} style={radioButtonStyle.radioButton} label={option.label} value={option.id} status={valueGroup2 === option.id ? 'checked' : 'unchecked'} onPress={() => handlePressGroup2(option)} />
                                     </View>
                                 </View>
-                                <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'right' , marginTop:50}}>RIMS SELECTION</Text>
-                               
-                                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', width: 400,height:180 }}>
-                                    {optionsGroup2.map((option) => (
-                                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                                            <View style={{position:'relative'}}>
-                                            <Image source={{ uri: rims.find(rim => rim._id === option.id)?.photo_select }} alt={colors[0]?.name} style={{ width: 120, height: 120, marginTop: -30 , position:'absolute',marginLeft:-25}}  />
-                                                <RadioButton.Item key={option.id} style={radioButtonStyle.radioButton} label={option.label} value={option.id} status={valueGroup2 === option.id ? 'checked' : 'unchecked'} onPress={() => handlePressGroup2(option)} />
-                                                
-                                            </View>
-                                        </View>
-                                    ))}
-                                </View>
-                            </View> : null}
+                            ))}
+                        </View>
+                    </View> : null}
+            </View>
+        </View>
+        <View>
+            <Text style={{ backgroundColor: 'black', width: 420, color: 'white', padding: 15, textAlign: 'center', fontSize: 20, fontWeight: 600 }} >RESUME</Text>
+            <View style={{ flexDirection: 'column', justifyContent: 'flex-end' }} >
+                <View>
+                    <Image source={photoVehicle} alt="Imagen de ejemplo" />
+                    <Text style={{ backgroundColor: 'white', width: 420, color: 'black', textAlign: 'center', fontSize: 20, padding: 8 }}>{car?.name}</Text>
+                    <Text style={{ backgroundColor: 'rgba(29,38,51,0.5)', width: 420, color: 'white', padding: 5, textAlign: 'center', fontSize: 15 }}>${(car?.price)?.toLocaleString("es-VE")}</Text>
+                    <Text style={{ backgroundColor: 'white', width: 420, color: 'black', textAlign: 'center', fontSize: 20, padding: 8 }}>{colors[parseInt(selectedOption.charAt(selectedOption.length - 1)) - 1]?.name}</Text>
+                    <Text style={{ backgroundColor: 'rgba(29,38,51,0.5)', width: 420, color: 'white', padding: 5, textAlign: 'center', fontSize: 15 }}>${(colors[parseInt(selectedOption.charAt(selectedOption.length - 1)) - 1]?.price_color)?.toLocaleString("es-VE")}</Text>
+                    <Text style={{ backgroundColor: 'white', width: 420, color: 'black', textAlign: 'center', fontSize: 20, padding: 8 }}>{rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 4]?.name}</Text>
+                    <Text style={{ backgroundColor: 'rgba(29,38,51,0.5)', width: 420, color: 'white', padding: 5, textAlign: 'center', fontSize: 15 }}>${(rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 4]?.price_rim)?.toLocaleString("es-VE")}</Text>
+                    <Text style={{ backgroundColor: 'white', width: 420, color: 'black', textAlign: 'center', fontSize: 20, padding: 8 }}>TOTAL</Text>
+                    <Text style={{ backgroundColor: 'rgba(29,38,51,0.5)', width: 420, color: 'white', padding: 5, textAlign: 'center', fontSize: 15 }}>${(car?.price + colors[parseInt(selectedOption.charAt(selectedOption.length - 1)) - 1]?.price_color + rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 4]?.price_rim)?.toLocaleString("es-VE")}</Text>
+                    <TouchableOpacity onPress={handleItem}><Text style={{ backgroundColor: 'white', width: 420, height: 60, color: 'black', padding: 5, textAlign: 'center', fontSize: 25 }}>ADD TO CART</Text></TouchableOpacity>
                 </View>
             </View>
-            <View>
-                <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'center',fontSize:15 }} >ADD TO RESUME</Text>
-                <View style={{ flexDirection: 'column', justifyContent: 'flex-end' }} >
-                    <View>
-                        <Image source={photoVehicle} alt="Imagen de ejemplo"/>
-                        <Text style={{ backgroundColor: 'grey', width: 420, height: 50, color: 'white', textAlign: 'center' ,fontSize:20,padding:8}}>{car?.name}</Text>
-                        <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'center',fontSize:15 }}>${(car?.price)?.toLocaleString("es-VE")}</Text>
-                        <Text style={{ backgroundColor: 'grey', width: 420, height: 50, color: 'white', textAlign: 'center',fontSize:20 ,padding:8}}>{colors[parseInt(selectedOption.charAt(selectedOption.length - 1)) - 1]?.name}</Text>
-                        <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'center',fontSize:15 }}>${(colors[parseInt(selectedOption.charAt(selectedOption.length - 1)) - 1]?.price_color)?.toLocaleString("es-VE")}</Text>
-                        <Text style={{ backgroundColor: 'grey', width: 420, height: 50, color: 'white', textAlign: 'center',fontSize:20 }}>{rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 1]?.name}</Text>
-                        <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'center',fontSize:15 }}>${(rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 1]?.price_rim)?.toLocaleString("es-VE")}</Text>
-                        <Text style={{ backgroundColor: 'grey', width: 420, height: 50, color: 'white', textAlign: 'center',fontSize:20,padding:8 }}>TOTAL</Text>
-                        <Text style={{ backgroundColor: 'black', width: 420, height: 30, color: 'white', padding: 5, textAlign: 'center',fontSize:15 }}>${(car?.price + colors[parseInt(selectedOption.charAt(selectedOption.length - 1)) - 1]?.price_color + rims[parseInt(selectedOptionRim.charAt(selectedOptionRim.length - 1)) - 1]?.price_rim)?.toLocaleString("es-VE")}</Text>
-                        <TouchableOpacity onPress={handleItem}><Text style={{ backgroundColor: 'white', width: 420, height: 60, color: 'black', padding: 5, textAlign: 'center',fontSize:25 }}>ADD TO CART</Text></TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </ScrollView>
-    )
+        </View>
+    </ScrollView>
+)
 }
